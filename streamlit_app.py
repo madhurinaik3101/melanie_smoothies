309ec1eb4aa4777360f307f47e6edc7313560aba
Madhuri Naik <37347515+madhurinaik3101@users.noreply.github.com>
2024-06-26 13:34:14 -0400

    Update streamlit_app.py
    Added Requests Python REST API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -34,5 +35,6 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered')
-    
-    
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)    
